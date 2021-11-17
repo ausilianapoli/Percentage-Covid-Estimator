9400834950e5a943851bdb03a8789b66e947865e
ausilianapoli <ausilianpoli@gmail.com>
2021-11-17 18:37:16 +0100

    fix extract label

--- CovidPerData.py
+++ CovidPerData.py
@@ -6,6 +6,7 @@ import numpy as np
 import random
 import pandas as pd
 from PIL import Image
+import cv2
 
 SLICE = 0
 PERCENTAGE = 1
@@ -23,9 +24,10 @@ class CovidPerData(Dataset):
         random.seed(1702)
         resize = 299 if inception else 224
         self.__adapter_transform = transforms.Compose([transforms.Resize(resize), transforms.ToTensor(), transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])])
-        self.__data_augmentation = transforms.Compose([transforms.RandomHorizontalFlip(),transforms.RandomResizedCrop(resize-4), transforms.Resize(resize), transforms.RandomRotation((-10, +10))])
+        self.__data_augmentation = transforms.Compose([transforms.RandomHorizontalFlip(),transforms.RandomResizedCrop(resize-10), transforms.Resize(resize), transforms.RandomRotation((-10, +10))])
         self.__read_data()
         self.__splitting_data()
+        self.__extract_data()
         self.__assign_data()
         
     def __read_data(self):
@@ -33,6 +35,12 @@ class CovidPerData(Dataset):
         self.__labels_path = os.path.join(self.__data_path, 'Train.csv')
         if os.path.exists(self.__labels_path):
             self.__Y = pd.read_csv(self.__labels_path, header = None) #it's a dataframe 
+
+    def __extract_data(self):
+        slices = self.__Y[SLICE].values
+        percentages = self.__Y[PERCENTAGE].values
+        data = {k:v for k,v in zip(slices, percentages)}
+        self.__Y = data
                     
     def __assign_data(self):
         if self.__mode == 'training':
@@ -49,12 +57,16 @@ class CovidPerData(Dataset):
                 
     def __getitem__(self, index):
         x = Image.open(self.__X[index]).convert('RGB')
+        #x = cv2.imread(self.__X[index])
+        #x = np.uint8(cv2.normalize(x, None, 0, 255, cv2.NORM_MINMAX)) #histogram equalization
+        #x = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        #x = Image.fromarray(x)
         x = self.__adapter_transform(x)
         if self.__mode == 'training':
             x = self.__data_augmentation(x)
         image_name = os.path.basename(self.__X[index])
-        y = float(self.__Y[self.__Y[SLICE] == image_name][PERCENTAGE])
-
+        y = self.__Y[image_name]
+        
         return x, y
     
     def __len__(self):
