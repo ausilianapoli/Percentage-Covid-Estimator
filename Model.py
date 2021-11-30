4ff682d0c866c7ba0dc6a4b7d153d5ca6b3d63bb
Maria Ausilia Napoli Spatafora <32179189+ausilianapoli@users.noreply.github.com>
2021-11-30 16:57:12 +0100

    add multiple inputs inceptionv3

--- Model.py
+++ Model.py
@@ -40,3 +40,21 @@ class ResNext(nn.Module):
     def forward(self, x):
         x = self.model(x)
         return x#self.sigmoid(x)
+    
+class InceptionV3Branches(nn.Module):
+
+    def __init__(self, dropout = 0.5, momentum = 0.9):
+        super(InceptionV3, self).__init__()
+        self.model = models.inception_v3(pretrained = True, progress = False, aux_logits = False)
+        self.model.fc = nn.Linear(2048, 1) #changing last layer
+        modules = list(self.model.children())
+        self.head = nn.Sequential(*modules[:7])
+        self.body = nn.Sequential(*modules[7:])
+        
+    def forward(self, x, x_he, x_clahe):
+        x = self.head(x)
+        x_he = self.head(x_he)
+        x_clahe = self.head(x_clahe)
+        x_mixed = torch.cat(x, x_he, x_clahe)
+        x_mixed = self.body(x_mixed)
+        return x_mixed
